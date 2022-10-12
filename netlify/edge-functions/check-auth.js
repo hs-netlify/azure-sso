59e523ed9526e61380bafb3ba3252c426f785eb1
Henry Smith <henry.smith@netlify.com>
2022-10-12 17:19:15 +0100

    Update to general url

--- netlify/edge-functions/check-auth.js
+++ netlify/edge-functions/check-auth.js
@@ -1,61 +1,69 @@
 const ssoAuth = async (request, context) => {
-  const { AZURE_AD_CLIENT_ID: clientId, AZURE_AD_TENANT_ID: tenantId } =
-    Deno.env.toObject();
+  const {
+    AZURE_AD_CLIENT_ID: clientId,
+    AZURE_AD_TENANT_ID: tenantId,
+    AZURE_AD_SECRET: secret,
+  } = Deno.env.toObject();
 
-  const redirectUri = "http://localhost:8888/";
+  if (clientId && tenantId && secret) {
+    const authToken = context.cookies.get("AAD_Token");
+    const url = new URL(request.url);
+    const path = url.pathname;
+    const params = new URLSearchParams(url.search);
+    const code = params.get("code");
 
-  const isValid = async (token) => {
-    const response = await fetch(`https://graph.microsoft.com/oidc/userinfo`, {
-      headers: {
-        Authorization: `bearer ${token}`,
-      },
-    });
+    const redirectUri = url.origin;
 
-    const data = await response.json();
-    return data.error ? false : true;
-  };
+    const isValid = async (token) => {
+      const response = await fetch(
+        `https://graph.microsoft.com/oidc/userinfo`,
+        {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        }
+      );
 
-  const authRedirect = () => {
-    return Response.redirect(
-      `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(
-        redirectUri
-      )}&response_mode=query&scope=https://graph.microsoft.com/openid&state=1234`
-    );
-  };
+      const data = await response.json();
+      return data.error ? false : true;
+    };
 
-  const authToken = context.cookies.get("AAD_Token");
-  const url = new URL(request.url);
-  const path = url.pathname;
-  const params = new URLSearchParams(url.search);
-  const code = params.get("code");
+    const authRedirect = () => {
+      return Response.redirect(
+        `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(
+          redirectUri
+        )}&response_mode=query&scope=https://graph.microsoft.com/openid&state=1234`
+      );
+    };
 
-  if (authToken) {
-    if (await isValid(authToken)) {
+    if (authToken) {
+      if (await isValid(authToken)) {
+        return context.next();
+      } else {
+        context.cookies.delete("AAD_Token");
+        const res = new Response(null, { status: 302 });
+        res.headers.set("Location", url.origin);
+        return res;
+      }
+    } else if (path === "/.netlify/functions/getToken") {
       return context.next();
-    } else {
-      context.cookies.delete("AAD_Token");
-      const res = new Response(null, { status: 302 });
-      res.headers.set("Location", "http://localhost:8888/");
-      return res;
-    }
-  } else if (path === "/.netlify/functions/getToken") {
-    return context.next();
-  } else if (code) {
-    const { access_token } = await (
-      await fetch(
-        `http://localhost:8888/.netlify/functions/getToken?code=${code}`
-      )
-    ).json();
-    if (access_token) {
-      context.cookies.set({ name: "AAD_Token", value: access_token });
-      const res = new Response(null, { status: 302 });
-      res.headers.set("Location", "http://localhost:8888/");
-      return res;
+    } else if (code) {
+      const { access_token } = await (
+        await fetch(`${url.origin}/.netlify/functions/getToken?code=${code}`)
+      ).json();
+      if (access_token) {
+        context.cookies.set({ name: "AAD_Token", value: access_token });
+        const res = new Response(null, { status: 302 });
+        res.headers.set("Location", url.origin);
+        return res;
+      } else {
+        return authRedirect();
+      }
     } else {
       return authRedirect();
     }
   } else {
-    return authRedirect();
+    return context.next();
   }
 };
 
